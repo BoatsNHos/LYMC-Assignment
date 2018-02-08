84d7d71f8a498f0e4f70f4b9e847fae081aa1d61
bryan <bryan@BRYANBROTONA0C4>
2018-02-08 10:16:08 -0800

    Add project files.

---
+++ lymcAssignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lymcAssignment.Startup))]
+namespace lymcAssignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
