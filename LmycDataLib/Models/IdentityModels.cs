6e86e01ba72f598b860ec2abbda0c6c3a90d38e0
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-02-15 21:26:00 -0800

    Added User and Boat models, closes #1

--- LmycDataLib/Models/IdentityModels.cs
+++ LmycDataLib/Models/IdentityModels.cs
@@ -4,11 +4,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace LmycWebSite.Models
+namespace LmycDataLib.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Id { get; internal set; }
+
+        public string FirstName { get; internal set; }
+
+        public string LastName { get; internal set; }
+
+        public string Street { get; internal set; }
+
+        public string City { get; internal set; }
+
+        public string Province { get; internal set; }
+
+        public string Country { get; internal set; }
+
+        public string PostalCode { get; internal set; }
+
+        public string PhoneNumber { get; internal set; }
+
+        public string SailingExperience { get; internal set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -21,7 +41,7 @@ namespace LmycWebSite.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("Lymc", throwIfV1Schema: false)
         {
         }
 
@@ -29,5 +49,8 @@ namespace LmycWebSite.Models
         {
             return new ApplicationDbContext();
         }
+        public DbSet<Boat> Boat { get; set; }
+
     }
+
 }
